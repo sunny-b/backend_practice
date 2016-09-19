cabb7b0baba19bae7da94d9a08ce056a6c7fb9e7
Sun-Li Beatteay <sunny@Sun-Lis-MBP.home>
2016-09-19 12:48:47 -0400

    Added solutions to Chapter Basics

---
+++ Basics/exercise_3_basics.rb
@@ -0,0 +1,5 @@
+movie_year = {"Jaws" => "1975", "The Incredibles" => "2004", "Man of Steel" => "2013", "Shrek" => "2001", "Raiders of the Lost Ark" => "1981"}
+
+movie_year.each do |movie, year|
+  puts year
+end
