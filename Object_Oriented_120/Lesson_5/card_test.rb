c604bd868e6da90b7e53e7dc8ecb29dbe268f009
Sunny <sjbeatteay@gmail.com>
2017-02-24 12:08:26 -0500

    add Ruby Practice

---
+++ Object_Oriented_120/Lesson_5/card_test.rb
@@ -0,0 +1,101 @@
+
+hand = PokerHand.new(Deck.new)
+hand.print
+puts hand.evaluate
+
+# Danger danger danger: monkey
+# patching for testing purposes.
+class Array
+  alias_method :draw, :pop
+end
+
+# Test that we can identify each PokerHand type.
+hand = PokerHand.new([
+  Card.new(10,      'Hearts'),
+  Card.new('Ace',   'Hearts'),
+  Card.new('Queen', 'Hearts'),
+  Card.new('King',  'Hearts'),
+  Card.new('Jack',  'Hearts')
+])
+puts hand.evaluate == 'Royal flush'
+
+hand = PokerHand.new([
+  Card.new(8,       'Clubs'),
+  Card.new(9,       'Clubs'),
+  Card.new('Queen', 'Clubs'),
+  Card.new(10,      'Clubs'),
+  Card.new('Jack',  'Clubs')
+])
+puts hand.evaluate == 'Straight flush'
+
+hand = PokerHand.new([
+  Card.new(3, 'Hearts'),
+  Card.new(3, 'Clubs'),
+  Card.new(5, 'Diamonds'),
+  Card.new(3, 'Spades'),
+  Card.new(3, 'Diamonds')
+])
+puts hand.evaluate == 'Four of a kind'
+
+hand = PokerHand.new([
+  Card.new(3, 'Hearts'),
+  Card.new(3, 'Clubs'),
+  Card.new(5, 'Diamonds'),
+  Card.new(3, 'Spades'),
+  Card.new(5, 'Hearts')
+])
+puts hand.evaluate == 'Full house'
+
+hand = PokerHand.new([
+  Card.new(10, 'Hearts'),
+  Card.new('Ace', 'Hearts'),
+  Card.new(2, 'Hearts'),
+  Card.new('King', 'Hearts'),
+  Card.new(3, 'Hearts')
+])
+puts hand.evaluate == 'Flush'
+
+hand = PokerHand.new([
+  Card.new(8,      'Clubs'),
+  Card.new(9,      'Diamonds'),
+  Card.new(10,     'Clubs'),
+  Card.new(7,      'Hearts'),
+  Card.new('Jack', 'Clubs')
+])
+puts hand.evaluate == 'Straight'
+
+hand = PokerHand.new([
+  Card.new(3, 'Hearts'),
+  Card.new(3, 'Clubs'),
+  Card.new(5, 'Diamonds'),
+  Card.new(3, 'Spades'),
+  Card.new(6, 'Diamonds')
+])
+puts hand.evaluate == 'Three of a kind'
+
+hand = PokerHand.new([
+  Card.new(9, 'Hearts'),
+  Card.new(9, 'Clubs'),
+  Card.new(5, 'Diamonds'),
+  Card.new(8, 'Spades'),
+  Card.new(5, 'Hearts')
+])
+puts hand.evaluate == 'Two pair'
+
+hand = PokerHand.new([
+  Card.new(2, 'Hearts'),
+  Card.new(9, 'Clubs'),
+  Card.new(5, 'Diamonds'),
+  Card.new(9, 'Spades'),
+  Card.new(3, 'Diamonds')
+])
+puts hand.evaluate == 'Pair'
+
+hand = PokerHand.new([
+  Card.new(2,      'Hearts'),
+  Card.new('King', 'Clubs'),
+  Card.new(5,      'Diamonds'),
+  Card.new(9,      'Spades'),
+  Card.new(3,      'Diamonds')
+])
+puts hand.evaluate == 'High card'
--- practice_files/fruit_method.rb
+++ practice_files/fruit_method.rb
@@ -1,9 +1,13 @@
-HEXADEMICAL = %w(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+HEXADEMICAL = %w(1 2 3 4 5 6 7 8 9 0 a b c d e f)  # => ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f"]
 
-def UUID
-  group_1 = 8.times.with_object("") do { |string| string << HEXADEMICAL.sample }
-  group_2
-  group_3
-  group_4
-  group_5
-end
+def uuid
+  group_1 = 8.times.with_object("") { |_, string| string << HEXADEMICAL.sample }   # => "0a580bd3"
+  group_2 = 4.times.with_object("") { |_, string| string << HEXADEMICAL.sample }   # => "ad8e"
+  group_3 = 4.times.with_object("") { |_, string| string << HEXADEMICAL.sample }   # => "4aa1"
+  group_4 = 4.times.with_object("") { |_, string| string << HEXADEMICAL.sample }   # => "b825"
+  group_5 = 12.times.with_object("") { |_, string| string << HEXADEMICAL.sample }  # => "6442e98450ac"
+
+  "#{group_1}-#{group_2}-#{group_3}_#{group_4}-#{group_5}"  # => "0a580bd3-ad8e-4aa1_b825-6442e98450ac"
+end                                                         # => :uuid
+
+uuid  # => "0a580bd3-ad8e-4aa1_b825-6442e98450ac"
